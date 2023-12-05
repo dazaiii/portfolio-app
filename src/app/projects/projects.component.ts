976efcece5f33de7f4741aae30bf911412d3a136
dazaiii <kin.baryczka@gmail.com>
2023-12-05 17:44:31 +0100

    Margins fixes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
-  public images: string[] = [
-    '../../assets/spacex.png',
-    '../../assets/quizapp.png',
-  ];
-
-  public responsiveOptions: any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5,
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3,
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1,
-    },
-  ];
-
-  ngOnInit(): void {
-    this.images = [];
-  }
-}
+export class ProjectsComponent {}
